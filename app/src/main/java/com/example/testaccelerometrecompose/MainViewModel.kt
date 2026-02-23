9fe9a85caa3029a6ae49f894fa32a202b0593102
Mireia Terricabras Vilardell <mtvilardell@gmail.com>
2026-02-23 21:48:22 +0100

    last commit together :CCCCC

--- app/src/main/java/com/example/testaccelerometrecompose/MainViewModel.kt
+++ app/src/main/java/com/example/testaccelerometrecompose/MainViewModel.kt
@@ -1,16 +1,18 @@
 package com.example.testaccelerometrecompose
 
+import android.app.Application
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val _color: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val color = _color.asStateFlow()
 
     init {
-        val sensorManager = AccelerometerSensorManager(this)
+        val sensorManager = AccelerometerSensorManager(application)
     }
 }
